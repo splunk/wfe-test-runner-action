13b44b76daddff987300e3ce9cf1d1b278a51640
Siddharth Khatsuriya <skhasturiya@splunk.com>
2021-08-18 00:02:45 +0530

    added -l before labels

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,13 +15,13 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #   ######################################################################## 
+
 RANDOM_STRING=`head -3 /dev/urandom | tr -cd '[:lower:]' | cut -c -4`
-echo 'RANDOM_STRING: ${RANDOM_STRING}'
 JOB_NAME="$5-$RANDOM_STRING"
 
 WORKFLOW_NAME=`argo submit -o json --from wftmpl/$1 -n $2 -l workflows.argoproj.io/workflow-template=$1 \
     -p ci-repository-url="git@github.com:${GITHUB_REPOSITORY}.git" -p ci-commit-sha=${GITHUB_SHA} -p run-destroy=$3 \
-    -p addon-url="$4" -p job-name=${JOB_NAME} -p splunk-version=$6 -p test-type=$7 -p test-args="$8" $9 | jq -r .metadata.name`
+    -p addon-url="$4" -p job-name=${JOB_NAME} -p splunk-version=$6 -p test-type=$7 -p test-args="$8" -l="$9" | jq -r .metadata.name`
 echo "After argo submit $?"
 echo 'WORKFLOW_NAME:' ${WORKFLOW_NAME}
 echo "::set-output name=workflow-name::$(echo $WORKFLOW_NAME)"
