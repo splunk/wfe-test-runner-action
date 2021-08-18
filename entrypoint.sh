98413cc904a63619bdf8bcb18ef488740be29575
uoboda-splunk <uoboda@splunk.com>
2021-08-18 14:15:12 +0200

    removing random string from job name

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,9 @@
 #   ######################################################################## 
 env | grep ARGO
 
-RANDOM_STRING=`head -3 /dev/urandom | tr -cd '[:lower:]' | cut -c -4`
-JOB_NAME="${5}-$RANDOM_STRING"
-
 WORKFLOW_NAME=`argo submit -v -o json --from wftmpl/${1} -n ${2} -l workflows.argoproj.io/workflow-template=${1} --argo-base-href '' \
     -p ci-repository-url="git@github.com:${GITHUB_REPOSITORY}.git" -p ci-commit-sha=${GITHUB_SHA} -p run-destroy=${3} \
-    -p addon-url="${4}" -p job-name=${JOB_NAME} -p splunk-version=${6} -p test-type=${7} -p test-args="${8}" -l="${9}" | jq -r .metadata.name`
+    -p addon-url="${4}" -p job-name=${5} -p splunk-version=${6} -p test-type=${7} -p test-args="${8}" -l="${9}" | jq -r .metadata.name`
 
 echo "After argo submit $?"
 echo 'WORKFLOW_NAME:' ${WORKFLOW_NAME}
