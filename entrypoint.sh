f71c5d6386ef9ad4a1307a303d0b7a028d361bba
Siddharth Khatsuriya <62132600+siddharthkhatsuriya-crest@users.noreply.github.com>
2021-10-12 12:35:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ else
 fi
 
 WORKFLOW_NAME=`argo submit -v -o json --from wftmpl/${1} -n ${2} -l workflows.argoproj.io/workflow-template=${1} --argo-base-href '' \
-    -p ci-repository-url="git@github.com:${GITHUB_REPOSITORY}.git" \
+    -p ci-repository-url="git@github.com:${13}.git" \
     -p ci-commit-sha=${BRANCH_NAME} -p delay-destroy=${3} \
     -p addon-url="${4}" \
     -p job-name=${5} \
