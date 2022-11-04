18240cc6f4c8e3ce6b8fe320bdf6bd7eb62763c1
mkolasinski-splunk <105011638+mkolasinski-splunk@users.noreply.github.com>
2022-11-04 18:39:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [[ "${GITHUB_EVENT_NAME}" != "pull_request" ]]
 then
     BRANCH_NAME=${GITHUB_REF}
 else
-    BRANCH_NAME=${GITHUB_HEAD_REF}
+    BRANCH_NAME=${GITHUB_SHA}
 fi
 
 WORKFLOW_NAME=`argo submit -v -o json --from wftmpl/${1} -n ${2} -l workflows.argoproj.io/workflow-template=${1} --argo-base-href '' \
