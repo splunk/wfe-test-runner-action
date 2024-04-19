29384939baa6ba12e1b98bd84e7dca16905b6e72
pbajaj-crest <pbajaj@splunk.com>
2024-04-19 15:55:38 +0530

    feat!: remove the delay_destroy functionality and test k8s branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,21 +26,21 @@ fi
 
 WORKFLOW_NAME=`argo submit -v -o json --from wftmpl/${1} -n ${2} -l workflows.argoproj.io/workflow-template=${1} --argo-base-href '' \
     -p ci-repository-url="https://github.com/${GITHUB_REPOSITORY}.git" \
-    -p ci-commit-sha=${BRANCH_NAME} -p delay-destroy=${3} \
-    -p addon-url="${4}" \
-    -p job-name=${5} \
-    -p splunk-version=${6} \
-    -p test-type=${7} \
-    -p k8s-manifests-branch="${8}" \
-    -p pytest-args="${9}" \
-    -p addon-name=${11} \
-    -p vendor-version=${12}\
-    -p sc4s-version=${13} \
-    -p install-java=${14} \
-    -p sc4s-docker-registry=${15} \
-    -p os-name=${16} \
-    -p os-version=${17} \
-    -l="${10},test-type=${7},splunk-version=${6}" | jq -r .metadata.name`
+    -p ci-commit-sha=${BRANCH_NAME} \
+    -p addon-url="${3}" \
+    -p job-name=${4} \
+    -p splunk-version=${5} \
+    -p test-type=${6} \
+    -p k8s-manifests-branch="${7}" \
+    -p pytest-args="${8}" \
+    -p addon-name=${10} \
+    -p vendor-version=${11}\
+    -p sc4s-version=${12} \
+    -p install-java=${13} \
+    -p sc4s-docker-registry=${14} \
+    -p os-name=${15} \
+    -p os-version=${16} \
+    -l="${9},test-type=${6},splunk-version=${5}" | jq -r .metadata.name`
 
 echo "After argo submit $?"
 echo 'WORKFLOW_NAME:' ${WORKFLOW_NAME}
